23b3acb4bb07500e1c702c20578ee079164a3f05
Tim Warrington <tim.warrin@gmail.com>
2019-04-20 18:25:32 -0600

    add page handler

---
+++ internal/http/page_handler.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+// PageHandler ...
+type PageHandler struct{}
+
+// Routes creates a REST router for the page handler.
+func (p *PageHandler) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	// Load middleware specific to this router.
+	// r.Use()
+
+	r.Get("/*", p.Index)
+
+	return r
+}
+
+// Index responds to a request for the site index page.
+func (p *PageHandler) Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("<h1>Page#Index</h1>"))
+}
